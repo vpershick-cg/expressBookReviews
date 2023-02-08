c0107ee306087e7389af6ecac6b18f59dda708ac
My Name <me@example.com>
2023-02-08 09:35:02 -0500

    IBM NodeJS final lab, completed Tasks 10-13

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -27,53 +27,63 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-    return res.status(300).json({message: JSON.stringify(books)});
+    const get_books = new Promise((resolve, reject) => {
+        resolve(res.send(JSON.stringify({books}, null, 4)));
+      });
+
+      get_books.then(() => console.log("Promise for Task 10 resolved"));
 });
+
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
     const isbn = req.params.isbn;
-    let book_results_key = "None found";
-    let book_results_val = [];
-    Object.keys(books).forEach(function(key) {
-        //iterate, then upon finding the match, push it onto book_results
-        var val = books[key];
-        if(val.isbn === isbn) {
-            book_results_key = `Index ${key}:`;
-            book_results_val.push(`${book_results_key} ${JSON.stringify(books[key])}\n`);
-        }
+    const get_books = new Promise((resolve, reject) => {
+        let books_by_isbn = [];
+        Object.keys(books).forEach(function(key) {
+            var val = books[key];
+            if(val.isbn === isbn) {
+                books_by_isbn.push(`${JSON.stringify(books[key])}`);
+            }
+        });
+        resolve(res.send(JSON.stringify({books_by_isbn}, null, 4)));
       });
-    res.send(`Result for ISBN ${isbn}: \n----\n ${book_results_val}`);
+
+      get_books.then(() => console.log("Promise for Task 11 resolved"));
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
     const author = req.params.author;
-    let book_results_key = "None found";
-    let book_results_val = [];
-    Object.keys(books).forEach(function(key) {
-        var val = books[key];
-        if(val.author === author) {
-            book_results_key =  `Index ${key}:`;
-            book_results_val.push(`${book_results_key} ${JSON.stringify(books[key])}\n`);
-        }
+    const get_books = new Promise((resolve, reject) => {
+        let books_by_author = [];
+        Object.keys(books).forEach(function(key) {
+            var val = books[key];
+            if(val.author === author) {
+                books_by_author.push(`${JSON.stringify(books[key])}`);
+            }
+        });
+        resolve(res.send(JSON.stringify({books_by_author}, null, 4)));
       });
-    res.send(`Result for author ${author}: \n ----\n ${book_results_val}`);
+
+      get_books.then(() => console.log("Promise for Task 12 resolved"));
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
     const title = req.params.title;
-    let book_results_key = "None found";
-    let book_results_val = [];
-    Object.keys(books).forEach(function(key) {
-        var val = books[key];
-        if(val.title === title) {
-            book_results_key = `Index ${key}:`;
-            book_results_val.push(`${book_results_key} ${JSON.stringify(books[key])}\n`);
-        }
+    const get_books = new Promise((resolve, reject) => {
+        let books_by_title = [];
+        Object.keys(books).forEach(function(key) {
+            var val = books[key];
+            if(val.title === title) {
+                books_by_title.push(`${JSON.stringify(books[key])}`);
+            }
+        });
+        resolve(res.send(JSON.stringify({books_by_title}, null, 4)));
       });
-    res.send(`Result for title ${title}: \n ----\n ${book_results_val} \n`);
+
+      get_books.then(() => console.log("Promise for Task 13 resolved"));
 });
 
 //  Get book review
